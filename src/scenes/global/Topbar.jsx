15e13c2d57ef4f6478d9a7b76d16c872133cb62b
MrKazuma <touhidkabir17@gmail.com>
2025-01-30 16:44:12 +0530

    addfiles

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,52 @@
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase  from '@mui/material/InputBase';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Topbar=() => {
+
+    const theme = useTheme();
+    const colors=tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
+        {/* Search Bar */}
+        <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px">
+            <InputBase sx={{ml:2,flex:1}} placeholder="Search" />
+            <IconButton type="button" sx={{p:1}}>
+                <SearchIcon/>
+                </IconButton>
+        </Box>
+
+        {/* Icons */}
+        <Box display="flex">
+            <IconButton onClick={colorMode.toggle}>
+                {theme.palette.mode === 'dark' ? <DarkModeOutlinedIcon/> : <LightModeOutlinedIcon/>}
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon/>
+            </IconButton>
+        </Box>
+        
+
+    </Box>;
+}
+
+export default Topbar;
